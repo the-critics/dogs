327e52c9dfcc5c0de15bb7564f6b3524642c43e5
villetejohn <spinnerjohnv@gmail.com>
2018-05-27 11:15:19 +0800

    Added random image api

--- js/script.js
+++ js/script.js
@@ -3,51 +3,39 @@ $(function() {
 
 	var RandomImage = function() {
 
-		this.getImage = function( api ) {
+		this.getImage = function( imgClass, api ) {
 
 			$.getJSON(api, function( data ) {
-							this.imageLink = data.message;
-							this.getLink(this.imageLink);
+
+				$(`#${imgClass}`).attr('src', data.message);
+
 			});
-
 		};
 
-		this.getLink = function([]) {
-
-		};
-
-
-		
 	};		
-		
 
 	$.getJSON('https://dog.ceo/api/breeds/list/all', function( data ) {
 		$.each(data.message, function(dogBreed, dogSubBreed) {
-
 			if(dogBreed.length > 0) {
 
 				var newDog = new RandomImage();
-
-				var getRandomImage = newDog.getImage(`https://dog.ceo/api/breed/${dogBreed}/images/random`);
-				// console.log(newDog.getLink);
+				var getRandomImage = newDog.getImage(dogBreed, `https://dog.ceo/api/breed/${dogBreed}/images/random`);
 
 				$('.body-container').append(
 						`<div class="col-md-4 ">
 	              <div class="card mb-4 box-shadow">
-	                <img class="card-img-top" src="" alt="${dogBreed}">
+	                <img class="card-img-top" src="" alt="${dogBreed}" id="${dogBreed}">
 	                <div class="card-body">
 	                  <p class="card-text">${dogBreed}</p>
 	                  <div class="d-flex justify-content-between align-items-center">
 	                    <div class="btn-group">
 	                      <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
-	                      <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
 	                    </div>
-	                    <small class="text-muted">9 mins</small>
+	                    <small class="text-muted">${dogBreed.length} sub breeds </small>
 	                  </div>
 	                </div>
 	              </div>
 	            </div>`);
-
 			}
 
 		});
